357d7933b026e59bbf0e0167f43ca586281d7a12
arthis-12 <seetha2@rpi.edu>
2021-10-08 00:40:45 -0400

    uncommented js file

--- lab4.js
+++ lab4.js
@@ -4,16 +4,15 @@ $(document).ready(function() {
         url: "lab4.json",
         dataType: "json",
         success: function(response){
-            /*
+
             var message = "";
             
             $.each(responseData.text, function(i, item) {
                 $.each(item.tutoring-text, function(x, s) {
                     message += s;  
                 });     
-            });  */
+            });  
             $("#tutoring-text").html(message);
-            console.log("success");
         }, 
         error: function(msg) {
               // there was a problem
